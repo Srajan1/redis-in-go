9d99f4a0db2af0af8cc4d5c179c44521375bbda6
Srajan1 <srakishor.g@gmail.com>
2025-11-19 08:36:42 +0530

    SET and GET implemented

--- handler.go
+++ handler.go
@@ -1,8 +1,15 @@
 package main
 
+import "sync"
+
 var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
+	"SET":  set,
+	"GET":  get,
 }
+
+var redisMap = map[string]string{}
+var redisMutex = sync.RWMutex{}
 
 func ping(args []Value) Value {
 	if len(args) == 0 {
@@ -10,4 +17,36 @@ func ping(args []Value) Value {
 	}
 
 	return Value{typ: "string", str: args[0].bulk}
+}
+
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'set' command"}
+	}
+	key := args[0].bulk
+	value := args[1].bulk
+
+	redisMutex.Lock()
+	redisMap[key] = value
+	redisMutex.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'get' command"}
+	}
+
+	key := args[0].bulk
+
+	redisMutex.RLock()
+	val, ok := redisMap[key]
+	redisMutex.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "string", str: val}
 }
